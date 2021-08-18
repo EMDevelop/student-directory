106243df2a4b00062d6d37d8bcdec32ef24354de
EMDevelop <edwardmartin.inbox@gmail.com>
2021-08-18 11:07:12 +0100

    refactored code into smaller methods

--- directory.rb
+++ directory.rb
@@ -1,20 +1,17 @@
+@cohorts = ["january","february" ,"march","april", "may", "june", "july", "august", "september", "october", "november", "december"]
+@students = []
 
-def input_students(cohorts)
+def input_students
 
   puts "To finish, just hit return twice at any stage"
-  students = []
-  
   name = nil
-  
   while true do
-    
     puts "Please enter the names of the students"
     name = gets.chomp
-
     puts "Enter your cohort month, e.g. January, December"
     cohort = gets.chomp.downcase
-    until cohorts.include?(cohort) do 
-      if cohorts.include?(cohort)
+    until @cohorts.include?(cohort) do 
+      if @cohorts.include?(cohort)
         break
       else
         puts "you've entered an incorrect cohort month, try again please:"
@@ -24,14 +21,11 @@ def input_students(cohorts)
 
     puts "Please enter Hobby"
     hobby = gets.chomp
-
     puts "Please enter Birth Country"
     birth_country = gets.chomp
+    @students << {name: name, cohort: cohort.to_sym, hobby: hobby, birth_country: birth_country}
+    @students.count == 1 ? "Now we have 1 student" : "Now we have #{@students.count} students"
 
-    students << {name: name, cohort: cohort.to_sym, hobby: hobby, birth_country: birth_country}
-    students.count == 1 ? "Now we have 1 student" : "Now we have #{students.count} students"
-    print students
-    
     puts "Are you done adding students? y = quit, anything else carry on "
     quit = gets.chomp.downcase
     if quit == "y"
@@ -39,7 +33,7 @@ def input_students(cohorts)
     end
 
   end
-  students
+  @students
 end
 
 def print_header  
@@ -47,15 +41,15 @@ def print_header
   puts "-------------".center(20)
 end 
 
-def print_names(students,cohorts)
-  if students.length == 0
+def print_names
+  if @students.length == 0
     return
   end
   acc = 0
-  while acc < cohorts.length do
-    puts "Students enrolled in our #{cohorts[acc].capitalize!} cohort:"
-    students.map { |student|
-      if student[:cohort] == cohorts[acc].downcase.to_sym
+  while acc < @cohorts.length do
+    puts "Students enrolled in our #{@cohorts[acc].capitalize!} cohort:"
+    @students.map { |student|
+      if student[:cohort] == @cohorts[acc].downcase.to_sym
         puts student[:name]
       end
     }
@@ -64,31 +58,39 @@ def print_names(students,cohorts)
 
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu
-  students = []
-  cohorts = ["january","february" ,"march","april", "may", "june", "july", "august", "september", "october", "november", "december"]
-  loop do
+def print_menu
     puts "1. Input the students"
     puts "2. Show the students"
     puts "9. Exit" 
-    selection = gets.chomp
+end
 
-    case selection
+def show_students
+  print_header
+  print_names
+  print_footer
+end
+
+def process(selection)
+  case selection
     when "1"
-      students = input_students(cohorts)    
+      input_students   
     when "2"
-      print_header
-      print_names(students, cohorts)
-      print_footer(students)
+      show_students
     when "9"
       exit 
     else
       puts "I don't know what you meant, try again"
-    end
+  end
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 
